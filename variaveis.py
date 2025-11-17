f53520f4882fa85b687f3a15883d057870824edd
MatheusPonte0728 <157135406+MatheusPonte0728@users.noreply.github.com>
2025-11-17 16:56:58 -0300

    correção de estrutura

--- variaveis.py
+++ variaveis.py
@@ -3,7 +3,8 @@
 # Uma variável é um espaço na memória onde armazenamos um valor.
 
 
-nome = 'Matheus' # Variável do tipo string (texto), sempre entre aspas ("" ou '')
+# Variável do tipo string (texto), sempre entre aspas ("" ou '')
+nome = 'Matheus'
 idade = 27       # Var do tio inteiro(núm sem casas decimais)
 altura = 1.80    # Var do tipo float(núm com casas decimais)
 dev = True       # Var do tipo booleano, valores lógicos (true/false)
@@ -11,8 +12,9 @@ dev = True       # Var do tipo booleano, valores lógicos (true/false)
 # print(f'Olá, {nome}! Você tem {idade} anos e mede {altura}m.')
 
 
-nome = input('Digite seu nome: ') # Entrada de textp
-idade = int(input('Digite dua idade: ')) # Entrada de texto convertida para int
-altura = float(input('DIgite sua altura: ')) # Entrada convertida para float
+nome = input('Digite seu nome: ')  # Entrada de textp
+# Entrada de texto convertida para int
+idade = int(input('Digite dua idade: '))
+altura = float(input('DIgite sua altura: '))  # Entrada convertida para float
 
 print(f'Olá, {nome}! Você tem {idade} anos e mede {altura}m. ')
