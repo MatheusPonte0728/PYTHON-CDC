c6cc8d7be6713b74f1e20c30a00012469e6183c7
MatheusPonte0728 <157135406+MatheusPonte0728@users.noreply.github.com>
2025-11-17 16:43:26 -0300

    deixando as variáveis mais dinâmicas

--- variaveis.py
+++ variaveis.py
@@ -8,4 +8,11 @@ idade = 27       # Var do tio inteiro(núm sem casas decimais)
 altura = 1.80    # Var do tipo float(núm com casas decimais)
 dev = True       # Var do tipo booleano, valores lógicos (true/false)
 
-print(f'Olá, {nome}! Você tem {idade} anos e mede {altura}m.')
+# print(f'Olá, {nome}! Você tem {idade} anos e mede {altura}m.')
+
+
+nome = input('Digite seu nome: ') # Entrada de textp
+idade = int(input('Digite dua idade: ')) # Entrada de texto convertida para int
+altura = float(input('DIgite sua altura: ')) # Entrada convertida para float
+
+print(f'Olá, {nome}! Você tem {idade} anos e mede {altura}m. ')
