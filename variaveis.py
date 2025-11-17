811cbb7e6a749a3d083d33541a238b201eba8f2c
MatheusPonte0728 <157135406+MatheusPonte0728@users.noreply.github.com>
2025-11-17 11:52:25 -0300

    segundo comando python
    Variáveis e tipos de dados

---
+++ variaveis.py
@@ -0,0 +1,11 @@
+# Variáveis e tipos de dados 'básicos'
+
+# Uma variável é um espaço na memória onde armazenamos um valor.
+
+
+nome = 'Matheus' # Variável do tipo string (texto), sempre entre aspas ("" ou '')
+idade = 27       # Var do tio inteiro(núm sem casas decimais)
+altura = 1.80    # Var do tipo float(núm com casas decimais)
+dev = True       # Var do tipo booleano, valores lógicos (true/false)
+
+print(f'Olá, {nome}! Você tem {idade} anos e mede {altura}m.')
