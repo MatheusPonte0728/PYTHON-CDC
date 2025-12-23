75c1be710e44b12a0206c1a5d72a1ab82bbf6dfb
MatheusPonte0728 <157135406+MatheusPonte0728@users.noreply.github.com>
2025-12-23 20:30:32 -0300

    Atualizar o variaveis.py
    Adicionei cidade

--- variaveis.py
+++ variaveis.py
@@ -16,5 +16,7 @@ nome = input('Digite seu nome: ')  # Entrada de textp
 # Entrada de texto convertida para int
 idade = int(input('Digite dua idade: '))
 altura = float(input('DIgite sua altura: '))  # Entrada convertida para float
+cidade= input('Digite sua cidade')
 
-print(f'Olá, {nome}! Você tem {idade} anos e mede {altura}m. ')
+
+print(f'Olá, {nome}! Você tem {idade} anos, mora em {cidade} e mede {altura}m. ')
